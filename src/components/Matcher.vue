fa002df22ab7674ebd858150ced7cb1936d25e00
Tudor Rotarus <tudor.rotarus@qumu.com>
2018-10-02 13:33:35 +0100

    Added word highlighting

--- src/components/Matcher.vue
+++ src/components/Matcher.vue
@@ -2,11 +2,37 @@
   <div class="matcher">
     <h2>Mariana's tags matcher</h2>
     <div class="inputs-container">
-      <textarea v-model="input1"/>
-      <textarea v-model="input2"/>
+      <div class="container">
+        <div class="backdrop">
+          <div class="highlights">
+            <span
+              v-for="(word, index) in wordsLeft"
+              :key="index"
+              :class="{ 'mark': matches.indexOf(word) !== -1 }"
+            >
+              {{ word }},
+            </span>
+          </div>
+        </div>
+        <textarea :value="wordsLeft.join(', ')" @input="changeWordsLeft"/>
+      </div>
+      <div class="container">
+        <div class="backdrop">
+          <div class="highlights">
+            <span
+                v-for="(word, index) in wordsRight"
+                :key="index"
+                :class="{ 'mark': matches.indexOf(word) !== -1 }"
+            >
+              {{ word }},
+            </span>
+          </div>
+        </div>
+        <textarea :value="wordsRight.join(', ')" @input="changeWordsRight"/>
+      </div>
     </div>
     <div class="result">
-      {{ matches }}
+      {{ matches.join(', ') }}
     </div>
   </div>
 </template>
@@ -16,27 +42,21 @@ export default {
   name: 'Matcher',
   data() {
     return {
-      input1: '',
-      input2: '',
+      wordsLeft: [],
+      wordsRight: [],
     };
   },
   computed: {
     matches() {
-      if (!this.input1 || !this.input2) {
-        return '';
-      }
-
       const result = [];
-      const inputArray1 = this.parseInput(this.input1);
-      const inputArray2 = this.parseInput(this.input2);
 
-      inputArray1.forEach((element) => {
-        if (inputArray2.indexOf(element) !== -1) {
+      this.wordsLeft.forEach((element) => {
+        if (this.wordsRight.indexOf(element) !== -1) {
           result.push(element);
         }
       });
 
-      return result.join(', ');
+      return result;
     }
   },
   methods: {
@@ -45,6 +65,12 @@ export default {
         return element.trim().toLowerCase();
       });
     },
+    changeWordsLeft(event) {
+      this.wordsLeft = this.parseInput(event.target.value);
+    },
+    changeWordsRight(event) {
+      this.wordsRight = this.parseInput(event.target.value);
+    }
   },
 }
 </script>
@@ -61,13 +87,49 @@ export default {
     justify-content: space-between;
   }
 
-  .inputs-container textarea {
+  .inputs-container .container {
+    position: relative;
     flex-basis: 48%;
     height: 200px;
+  }
+
+  .inputs-container .container textarea {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    margin: 0;
+    padding: 5px;
+    border-radius: 0;
+    width: calc(100% - 6px);
+    color: #444; /* or whatever */
+    background-color: transparent;
+    font-size: 14px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
   }
 
   .result {
     margin-top: 2em;
     font-weight: bold;
   }
+
+  .backdrop {
+    position: absolute;
+    padding: 6px;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: auto;
+    color: transparent;
+    background-color: #fff;
+    font-size: 14px;
+    text-align: left;
+  }
+
+  .highlights .mark {
+    color: transparent;
+    background-color: #a0ff94; /* or whatever */
+  }
+
 </style>
